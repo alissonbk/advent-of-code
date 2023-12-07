acc812e380440e1c4db1412c46a03a015f95f099
alissonbk <alissonbk2010@hotmail.com>
2023-12-07 19:28:07 -0300

    day7 part one

--- 2023/day7/partone/partone.go
+++ 2023/day7/partone/partone.go
@@ -7,20 +7,11 @@ import (
 	"strings"
 )
 
-// const (
-//
-//	A = iota
-//	K
-//	Q
-//	J
-//	T
-//
-// )
 var CARDS = [5]string{"T", "J", "Q", "K", "A"}
 
 type Hand struct {
 	hand string
-	bid  uint16
+	bid  uint
 }
 
 func getCardStrength(card string) int {
@@ -35,14 +26,46 @@ func getCardStrength(card string) int {
 func getHandType(hand string) uint8 {
 	runes := []rune(hand)
 	var checkedRunes []rune
+	var equals []string
 	var numberEquals uint8 = 0
 	for _, r := range runes {
 		for _, checkedRune := range checkedRunes {
 			if r == checkedRune {
+				alreadyInEquals := false
+				if equals == nil {
+					equals = append(equals, string(r))
+				}
+				for _, e := range equals {
+					if string(r) == e {
+						alreadyInEquals = true
+					}
+				}
+				if !alreadyInEquals {
+					equals = append(equals, string(r))
+				}
 				numberEquals += 1
 			}
 		}
 		checkedRunes = append(checkedRunes, r)
+	}
+
+	if numberEquals == 10 {
+		return 7 // five of a kind
+	}
+	if numberEquals == 4 {
+		if len(equals) == 1 {
+			return 6 // four of a kind
+		}
+		if len(equals) == 2 {
+			return 5 // full house
+		}
+
+	}
+	if numberEquals == 3 {
+		if len(equals) == 1 {
+			return 4 // three of a kind
+		}
+
 	}
 	return numberEquals
 }
@@ -53,10 +76,7 @@ func isNumber(b byte) bool {
 
 func appendHandsOrdered(hands *[]Hand, hand Hand) {
 	handsLen := len(*hands)
-	//var newHands []Hand
 	newHandStr := []rune(hand.hand)
-	//posToAppend := 0
-	//foundFinalPosition := false
 	if handsLen == 0 {
 		*hands = append(*hands, hand)
 		return
@@ -65,13 +85,20 @@ func appendHandsOrdered(hands *[]Hand, hand Hand) {
 	for i := 0; i < handsLen; i++ {
 		handStr := []rune((*hands)[i].hand)
 
-		for idx := 0; idx < len(handStr); idx++ {
+		idx := 0
+		for {
 			oldHand := handStr[idx]
 			newHand := newHandStr[idx]
 			for oldHand == newHand {
-				idx++
-				oldHand = handStr[idx]
-				newHand = newHandStr[idx]
+				if idx+1 < len(handStr) {
+					idx++
+					oldHand = handStr[idx]
+					newHand = newHandStr[idx]
+				} else {
+					utils.InsertAtIndex(hands, i, hand)
+					return
+				}
+
 			}
 
 			if isNumber(byte(oldHand)) && isNumber(byte(newHand)) {
@@ -81,18 +108,30 @@ func appendHandsOrdered(hands *[]Hand, hand Hand) {
 					utils.InsertAtIndex(hands, i, hand)
 					return
 				} else {
-					//posToAppend = i + 1
+					if i == len(*hands)-1 {
+						*hands = append(*hands, hand)
+						return
+					}
+					break
 				}
 			} else if !isNumber(byte(oldHand)) && isNumber(byte(newHand)) {
 				utils.InsertAtIndex(hands, i, hand)
 				return
 			} else if isNumber(byte(oldHand)) && !isNumber(byte(newHand)) {
-				//posToAppend = i + 1
+				if i == len(*hands)-1 {
+					*hands = append(*hands, hand)
+					return
+				}
+				break
 			} else if getCardStrength(string(oldHand)) > getCardStrength(string(newHand)) {
 				utils.InsertAtIndex(hands, i, hand)
 				return
 			} else {
-				//posToAppend = i + 1
+				if i == handsLen-1 {
+					*hands = append(*hands, hand)
+					return
+				}
+				break
 			}
 
 		}
@@ -102,7 +141,6 @@ func appendHandsOrdered(hands *[]Hand, hand Hand) {
 func Run() {
 	rows := utils.GetSliceFromFile("/day7/partone/input.txt")
 	handMap := make(map[uint8][]Hand)
-	counter := uint16(1)
 	totalSum := 0
 	for _, row := range rows {
 		split := strings.Split(row, " ")
@@ -110,29 +148,26 @@ func Run() {
 		bid, _ := strconv.Atoi(split[1])
 		handType := getHandType(hand)
 		handsByType := handMap[handType]
-		appendHandsOrdered(&handsByType, Hand{hand: hand, bid: uint16(bid)})
-		//handsByType = append(handsByType, Hand{hand, uint16(bid)})
-		handMap[getHandType(hand)] = handsByType
+		appendHandsOrdered(&handsByType, Hand{hand: hand, bid: uint(bid)})
+		handMap[handType] = handsByType
 	}
 
-	fmt.Println(handMap)
-	for i := uint8(0); i < uint8(len(handMap)); i++ {
+	counter := uint(1)
+	i := uint8(0)
+	for i <= 7 {
 		hands := handMap[i]
 		if hands != nil {
 			if len(hands) == 1 {
 				totalSum += int(hands[0].bid * counter)
 				counter++
 			} else {
-				// should be ordered
-				//for _, hand := range hands {
-				//	var ordered []uint16
-				//
-				//}
+				for _, hand := range hands {
+					totalSum += int(hand.bid * counter)
+					counter++
+				}
 			}
-
 		}
-
+		i++
 	}
-
 	fmt.Println(totalSum)
 }
